282634f6f2400c914137066b578cc6942e5959ff
daelsaid <daelsaid@stanford.edu>
2018-10-09 20:37:45 +0200

    version control

--- dashboards/redcap_dashboard_generator.py
+++ dashboards/redcap_dashboard_generator.py
@@ -2,6 +2,8 @@
 
  #!/usr/bin/env python
 import pycurl, cStringIO
+import string
+import collections
 import time
 from itertools import groupby
 
@@ -48,16 +50,17 @@ report=buf.getvalue()
 ch.close()
 buf.close()
 
-current_date= str(time.strftime('%Y/%m/%d'))
-clinic_dict = {'1': 'Palo Alto' , '2': 'San Jose VA', '3': 'Monterey VA', '4': 'New Mexico VA', '5': 'Menlo Park',
-               '6': 'Fresno', '7': 'Livermore', '8': 'Stockton', '98': 'Other'}
 
 data_rep_pt = str(object=patient_report) #patient specific report
-data_rep_pt = data_rep_pt.replace(' ', '_').replace(', ', ' ').replace('record_id_redcap', 'id')
+data_rep_pt = data_rep_pt.replace(' ', '_')
+data_rep_pt = data_rep_pt.replace('record_id_redcap', 'id')
+data_rep_pt = data_rep_pt.replace(', ', ' ')
 data_test_pt = [data_rep_pt]
 
 data_rep = str(object=report)
-data_rep = data_rep.replace(' ', '_').replace(', ', ' ').replace('record_id_redcap', 'id')
+data_rep = data_rep.replace(' ', '_')
+data_rep = data_rep.replace('record_id_redcap', 'id')
+data_rep = data_rep.replace(', ', ' ')
 data_test = [data_rep]
 
 patient_corrected_list=[]
@@ -73,6 +76,11 @@ for test in data_test:
     for idx,val in enumerate(y):
         corrected_list.append(val.split(','))
 
+
+current_date= str(time.strftime('%Y/%m/%d'))
+clinic_dict = {'1': 'Palo Alto' , '2': 'San Jose VA', '3': 'Monterrey VA', '4': 'New Mexico VA', '5': 'Menlo Park',
+               '6': 'Fresno', '7': 'Livermore', '8': 'Stockton', '98': 'Other'}
+
 patient_referrals=0
 referall_date=0
 ineligible_patients_recruitment=0
@@ -87,9 +95,6 @@ freq_of_referrals=[]
 pt_cpt_therapy=0
 pt_therapy_still_unknown=0
 pt_pe_therapy=0
-dropped=0
-ready_tp2=0
-flwup=0
 
 scheduled_patient_tp1=0
 referred_patient_tp1=0
@@ -97,15 +102,10 @@ completed_patient_tp1=0
 scheduled_patient_tp2=0
 referred_patient_tp2=0
 completed_patient_tp2=0
-sched_tp2=0
-in_process_tp2 = 0
 
 total_subj_with_data_collected=0
 pt_weekly_count=0
 pt_upcoming_dates=[]
-
-hv_pending_screen=0
-hv_tp1_sched=0
 
 total_hc_tp1_scheduled=0
 total_hc_tp2_scheduled=0
@@ -117,7 +117,6 @@ weekly_count_tp2=0
 total_hc_tp2_complete =0
 total_hc_tp1_complete = 0
 total_run_subjects=0
-completed_all_therapy=0
 
 
 for pt in patient_corrected_list:
@@ -149,31 +148,18 @@ for pt in patient_corrected_list:
         pt_therapy_still_unknown = pt_therapy_still_unknown + 1
     if '1' in pt[8] and pt[5] !='0' and pt[5] != '':
         pt_pe_therapy = pt_pe_therapy + 1
-    if '1' in pt[9] and '1' not in pt[10]:
-        sched_tp2=sched_tp2+1
-    if '1' in pt[9] and '1' in pt[10]:
-        completed_patient_tp2=completed_patient_tp2 + 1
-    if pt[9] == '55':
-        in_process_tp2=in_process_tp2+1
-    if pt[12] == '1':
-        completed_all_therapy=completed_all_therapy + 1
-    if pt[12] == '0':
-        dropped = dropped +1
-    if pt[14] == '55':
-        flwup=flwup + 1
-    if pt[16] == '1' and pt[17] != '0':
-        ready_tp2=ready_tp2 + 1
 
 
 referrals_freqs= [(k,len(list(g))) for k, g in groupby(sorted(freq_of_referrals))]
 pending_eligibility=[(k,len(list(g))) for k, g in groupby(sorted(pending_eligibility_locations))]
 
+print pending_eligibility_locations
 for subjects in corrected_list:
     subjects[9] = subjects[9].replace('-', '/')
     subjects[10] = subjects[10].replace('-', '/')
     if 'tp1_day1_arm_1' in subjects[1]:
         scheduled=[]
-        tp2_scheduled=[]
+	   tp2_scheduled=[]
         referred=[]
         completed_tp1=[]
         completed_tp2=[]
@@ -184,7 +170,10 @@ for subjects in corrected_list:
         scheduled_patient_tp1 = scheduled_patient_tp1 + scheduled
         completed_tp1 = subjects[5].count('1')
         completed_patient_tp1 = completed_patient_tp1 + completed_tp1
-        tp2_sched_temp = subjects[4].count('1')
+        tp2_scheduled = subjects[4].count('1')
+        scheduled_patient_tp2 = scheduled_patient_tp2 + tp2_scheduled
+        completed_tp2 = subjects[6].count('1')
+        completed_patient_tp2 = completed_patient_tp2 + completed_tp2
         total_subj_with_data = subjects[11].count('1')
         total_subj_with_data_collected = total_subj_with_data_collected + total_subj_with_data
         pt_dates= subj[9]
@@ -197,12 +186,6 @@ for subjects in corrected_list:
         else:
             total_run= subjects[9].count('1')
             total_run_subjects = total_run_subjects + total_run
-    if 'arm_2' in subjects[1]:
-        subj_all=subjects[0:]
-        if subj_all[12] == '50':
-            hv_pending_screen = hv_pending_screen + 1
-        if subj_all[3] == '1':
-            hv_tp1_sched= hv_tp1_sched + 1
     if 'arm_3' in subjects[1]:
         scheduled=[]
         weekly=[]
@@ -210,13 +193,17 @@ for subjects in corrected_list:
         tp1_completed=[]
         tp2_completed=[]
         subj=subjects[0:]
-        scheduled = subjects[3].count('1')         #hc tp1 sched
+        #hc tp1 sched
+        scheduled = subjects[3].count('1')
         total_hc_tp1_scheduled = total_hc_tp1_scheduled + scheduled
-        scheduled = subjects[5].count('1')         #hc tp2 sched
+        #hc tp2 sched
+        scheduled = subjects[5].count('1')
         total_hc_tp2_scheduled = total_hc_tp2_scheduled + scheduled
-        tp1_completed = subjects[5].count('1')         #hc tp1 completed
+        #hc tp1 completed
+        tp1_completed = subjects[5].count('1')
         total_hc_tp1_complete = total_hc_tp1_complete + tp1_completed
-        tp2_completed = subjects[6].count('1')         #hc tp2 completed
+        #hc tp2 completed
+        tp2_completed = subjects[6].count('1')
         total_hc_tp2_complete = total_hc_tp2_complete + tp2_completed
         scheduled = subjects[4].count('1')
         dates_tp1= subj[9]
@@ -231,6 +218,7 @@ for subjects in corrected_list:
         if current_date <= dates_tp2:
             count_tp2 += 1
             weekly_count_tp2= weekly_count_tp2 + 1
+
 
 pt_upcoming_dates = sorted(pt_upcoming_dates)
 
@@ -260,35 +248,19 @@ print ''
 for pending in pending_eligibility:
     print pending
 print ''
+print "Total Number of patients receiving CPT: %s" %pt_cpt_therapy
+print "Total Number of patients receiving PE therapy: %s" %pt_pe_therapy
+print "Number of subjects that are still deciding between CPT and PE: %s" %pt_therapy_still_unknown
+print ''
 print "Total patients scheduled for Tp1: %s" %scheduled_patient_tp1
-print "Total Number of patients we have data for (Pt who have consented): %s" %total_subj_with_data_collected
+print "Total patients scheduled for Tp2: %s" %scheduled_patient_tp2
+print "Total Number of patients we have data for (signed consent): %s" %total_subj_with_data_collected
 print "Total Number of Tp1 completions: %s" %completed_patient_tp1
 print "Total upcoming Tp1 appointments (as of today's date): %s" %pt_weekly_count
+print "Total Number of Tp2 completions: %s" %completed_patient_tp2
 print ''
 for pt_date in upcoming_pt_dates:
     print pt_date
-print ''
-print ''
-
-print "Total Number of patients receiving CPT: %s" %pt_cpt_therapy
-print "Total Number of patients receiving PE therapy: %s" %pt_pe_therapy
-print "Number of patients that are still deciding between CPT and PE: %s" %pt_therapy_still_unknown
-print ''
-
-print "Number of patients ready for Session 5 Follow-Up Calls: %s" %flwup
-print "Number of patients that have completed therapy: %s" %completed_all_therapy
-print "Number of patients that dropped out of therapy: %s" %dropped
-print ''
-
-print "Number of patients ready to be scheduled for Tp2: %s" %ready_tp2
-print "Number of patients in process of being scheduled for Tp2: %s" %in_process_tp2
-print "Total patients scheduled for Tp2: %s" %sched_tp2
-print "Total Number of Tp2 completions: %s" %completed_patient_tp2
-print ''
-
-print '~~Healthy Veteran Numbers:'
-print ''
-print 'Vets pending online screen completion: %s' %hv_pending_screen
 print ''
 print ''
 

47001f534abda292202b6e3944ff0a266e81e359
daelsaid <daelsaid@stanford.edu>
2018-10-09 20:59:22 +0200

    version control

--- dashboards/redcap_dashboard_generator.py
+++ dashboards/redcap_dashboard_generator.py
@@ -1,178 +1,188 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Sun Jan 28 20:41:35 2018
+Created on Tue Mar 27 18:04:40 2018
 
 @author: dawlat_local
 """
 
-
 import pycurl
 import cStringIO
-import datetime as dt
-import time
-import sys
 import pandas
-from itertools import groupby
+import time
+from prettytable import PrettyTable
 
+#
+# curl -X POST -H "Cache-Control: no-cache" -F "token=" -F "record=" -F "format=json" -F "delta=96:00:00" "https://redcap.stanford.edu/plugins/open/last_modified.php"
+#
 
-report_test = '31113'
 current_date = str(time.strftime('%Y/%m/%d'))
+current_date.replace('/', '-')
 
-
-clinic_dict = {'1': 'Palo Alto', '2': 'San Jose VA', '3': 'Monterey VA', '4': 'New Mexico VA', '5': 'Menlo Park',
-               '6': 'Fresno', '7': 'Livermore', '8': 'Stockton', '9': 'Modesto', '10': 'Mather', '98': 'Other'}
-
-token = ''
-url = 'https://redcap.stanford.edu/api/'
-
-
-patient_buf = cStringIO.StringIO()
-patient_tracker_data = {
-    'token': token,
+buf = cStringIO.StringIO()
+data = {
+    'token': '',
     'content': 'report',
     'format': 'csv',
-    'report_id': report_test,
+    'report_id': '34176',
     'rawOrLabel': 'raw',
     'rawOrLabelHeaders': 'raw',
     'exportCheckboxLabel': 'false',
     'returnFormat': 'csv'
 }
+
 ch = pycurl.Curl()
-ch.setopt(ch.URL, url)
-ch.setopt(ch.HTTPPOST, patient_tracker_data.items())
-ch.setopt(ch.WRITEFUNCTION, patient_buf.write)
+ch.setopt(ch.URL, 'https://redcap.stanford.edu/api/')
+ch.setopt(ch.HTTPPOST, data.items())
+ch.setopt(ch.WRITEFUNCTION, buf.write)
 ch.perform()
-patient_report = patient_buf.getvalue()
 ch.close()
-patient_buf.close()
+redcap_output = buf.getvalue()
+buf.close()
 
-data_rep_pt = str(object=patient_report)
-data_rep_pt = data_rep_pt.replace(' ', '_').replace(
-    ', ', ' ').replace('record_id_redcap', 'id')
-data_test_pt = [data_rep_pt]
+prep_list = str(object=redcap_output)
+prepped_list = [prep_list.replace(' ', '_').replace('"', '')]
 
-data_rep = str(object=patient_report)
-data_rep = data_rep.replace(' ', '_').replace(
-    ', ', ' ').replace('record_id_redcap', 'id')
-data_test = [data_rep]
-
-patient_corrected_list = []
-for test in data_test_pt:
-    y = test.split()
-    for idx, val in enumerate(y):
-        patient_corrected_list.append(val.split(','))
-
-df = pandas.DataFrame(data=patient_corrected_list)
-column_headers = df[0:1].values.tolist()
-df.columns = column_headers
-df_corr_col_headers = df[1:]
-
-#####
+best_corrected = []
+for best_lines in prepped_list:
+    lines = best_lines.split()
+    for idx, val in enumerate(lines):
+        best_corrected.append(val.split(','))
 
 
-clinic = df_corr_col_headers.groupby('redcap_event_name')[
-    'if_a_patient_which_va_clin'].value_counts()
+numbers_df = pandas.DataFrame(data=best_corrected)
+best_columns = numbers_df[0:1].values.tolist()
+numbers_df.columns = best_columns
+numbers = numbers_df[1:]
 
-s5_sched = df_corr_col_headers.groupby('redcap_event_name')[
-    'session_5_followup'].value_counts()
-
-s5_call = df_corr_col_headers.groupby('redcap_event_name')[
-    'session_5_followup_call'].value_counts()
-
-pt_therapy = df_corr_col_headers.groupby('redcap_event_name')[
-    'if_a_patient_what_therapy_2'].value_counts()
-
-therapy_completed = df_corr_col_headers.groupby(
-    'redcap_event_name')['completed_all_therapy'].value_counts()
-
-pt_dropped_therapy = df_corr_col_headers.groupby(
-    'redcap_event_name')['why_therapy_not_completed___1'].value_counts()
-
-pt_dropped_unreachable = df_corr_col_headers.groupby(
-    'redcap_event_name')['why_therapy_not_completed___2'].value_counts()
-
-pt_switched_modals = df_corr_col_headers.groupby(
-    'redcap_event_name')['why_therapy_not_completed___3'].value_counts()
-
-pt_dropped_other = df_corr_col_headers.groupby(
-    'redcap_event_name')['why_therapy_not_completed___98'].value_counts()
-
-tp1_sched = df_corr_col_headers.groupby('redcap_event_name')[
-    'is_the_participant_confirm'].value_counts()
-
-tp2_sched = df_corr_col_headers.groupby('redcap_event_name')[
-    'is_the_participant_co2_2e3'].value_counts()
-
-tp1_complete = df_corr_col_headers.groupby('redcap_event_name')[
-    'time_point_completion_master'].value_counts()
-
-tp2_complete = df_corr_col_headers.groupby('redcap_event_name')[
-    'time_point_completion_master1'].value_counts()
-
-eligibile = df_corr_col_headers.groupby('redcap_event_name')[
-    'is_the_subject_eligable'].value_counts()
-
-ineligible_post_intake = df_corr_col_headers.groupby(
-    'redcap_event_name')['qc_ineligbile_screen_3'].value_counts()
-
-eligible_post_intake = df_corr_col_headers.groupby(
-    'redcap_event_name')['qc_eligbile_screen_2'].value_counts()
-
-withdrawn_withdrew = df_corr_col_headers.groupby(
-    'redcap_event_name')['qc_withdrew_status'].value_counts()
-
-eligibility_screen = df_corr_col_headers.groupby(
-    'redcap_event_name')['qc_eligbile_screen'].value_counts()
-
-screen_comp_status = df_corr_col_headers.groupby(
-    'redcap_event_name')['has_the_participant_comple'].value_counts()
+tp1_pt = []
+tp1_inprocess = []
+tp2_pt = []
+tp2_inprocess = []
+sixmonth = []
+tp1_hc = []
+tp2_hc = []
+hv_all = []
 
 
-print "Total Tp1 completions", tp1_complete[1]
-print "Total Tp2 completions", tp2_complete[1]
-print "Patients pending online screen completions", screen_comp_status.loc[
-    'tp1_day1_arm_1'][2]
+intake_tp1 = []
+
+for x, y in numbers.groupby('redcap_event_name'):
+    if y['enrolled_for'].values[4] == '1':
+        tp1_pt.append(y['date_of_tp2'].values.tolist())
+        sixmonth.append(y['six_month_followup_date'].values.tolist())
+        tp2_pt.append(y['date_of_tp2_appointment_2'].values.tolist())
+    if y['enrolled_for'].values[4] == '3':
+        tp1_hc.append(y['date_of_tp2'].values.tolist())
+        tp2_hc.append(y['date_of_tp2_appointment_2'].values.tolist())
+    else:
+        hv_all.append(y['date_of_tp2'].values.tolist())
+
+tp1_dates = [(tp1, val) for tp1, val in enumerate(tp1_pt)]
 
 
-print ''
-print "Total recieving PE:", tp1_sched.loc['tp1_day1_arm_1'][1] - \
-    pt_therapy[2], "/", tp1_complete[1]
-# DETERMINE IF WE INCLUDE PPL WHO HAVENT COPLETED TP1 YET
+tp1_completed_patients = numbers.groupby('redcap_event_name')[
+    'time_point_completion_master'].value_counts()[3]
+tp1_completed_hv = numbers.groupby('redcap_event_name')[
+    'time_point_completion_master'].value_counts()[7]
+tp1_completed_hc = numbers.groupby('redcap_event_name')[
+    'time_point_completion_master'].value_counts()[0]
 
-print "Total recieving CPT:", pt_therapy[1] - \
-    (tp1_sched.loc['tp1_day1_arm_1'][3]), "/", tp1_complete[1]
+tp2_completed_patients = numbers.groupby('redcap_event_name')[
+    'time_point_completion_master1'].value_counts()[4]
+tp2_completed_hc = numbers.groupby('redcap_event_name')[
+    'time_point_completion_master1'].value_counts()[0]
+six_month_followup = numbers.groupby('redcap_event_name')[
+    'six_month_followup'].value_counts()[2]
 
-print "Type of therapy still undecided:", pt_therapy[3], "/", tp1_complete[1]
+intake_pt_tp1 = numbers.groupby('redcap_event_name')[
+    'clinical_assessment_scores_complete'].value_counts()[2]
+tms_pt_tp1 = numbers.groupby('redcap_event_name')[
+    'tms_eeg_completed'].value_counts()[2]
+mri_pt_tp1 = numbers.groupby('redcap_event_name')[
+    'mri_completed_1'].value_counts()[2]
+saline_pt_tp1 = numbers.groupby('redcap_event_name')[
+    'did_the_participant_comple'].value_counts()[4]
+dry_pt_tp1 = numbers.groupby('redcap_event_name')[
+    'dry_eeg_completion'].value_counts()[4]
+webneuro_pt_tp1 = numbers.groupby('redcap_event_name')[
+    'webneuro_session_completed'].value_counts()[2]
+vm_react_pt_tp1 = numbers.groupby('redcap_event_name')[
+    'inquisit_task_completed'].value_counts()[2]
 
-print "Total recieving other form of therapy (Not CPT or PE):", pt_therapy[
-    4], "/", tp1_complete[1]
-print ''
+intake_pt_tp2 = numbers.groupby('redcap_event_name')[
+    'clinical_assessment_scores_complete'].value_counts()[4]
+vm_react_pt_tp2 = numbers.groupby('redcap_event_name')[
+    'inquisit_task_completed'].value_counts()[7]
+webneuro_pt_tp2 = numbers.groupby('redcap_event_name')[
+    'webneuro_session_completed'].value_counts()[8]
+tms_pt_tp2 = numbers.groupby('redcap_event_name')[
+    'tms_eeg_completed'].value_counts()[9]
+mri_pt_tp2 = numbers.groupby('redcap_event_name')[
+    'mri_completed_1'].value_counts()[8]
+dry_pt_tp2 = numbers.groupby('redcap_event_name')[
+    'dry_eeg_completion'].value_counts()[11]
+saline_pt_tp2 = numbers.groupby('redcap_event_name')[
+    'did_the_participant_comple'].value_counts()[10]
 
+saline_hc_tp1 = numbers.groupby('redcap_event_name')[
+    'did_the_participant_comple'].value_counts()[0]
 
-print "total completed S5 call", s5_sched.loc['tp1_day1_arm_1'][
-    1], "/", s5_sched.loc['tp1_day1_arm_1'][2] + s5_sched.loc['tp1_day1_arm_1'][1]
+intake_hv = numbers.groupby('redcap_event_name')[
+    'clinical_assessment_scores_complete'].value_counts()[7]
+webneuro_hv = numbers.groupby('redcap_event_name')[
+    'webneuro_session_completed'].value_counts()[5]
+vm_react_hv = numbers.groupby('redcap_event_name')[
+    'inquisit_task_completed'].value_counts()[4]
+tms_hv = numbers.groupby('redcap_event_name')[
+    'tms_eeg_completed'].value_counts()[5]
+mri_hv = numbers.groupby('redcap_event_name')[
+    'mri_completed_1'].value_counts()[5]
+saline_hv_tp1 = numbers.groupby('redcap_event_name')[
+    'did_the_participant_comple'].value_counts()[6]
+dry_hv_tp1 = numbers.groupby('enrolled_for')[
+    'dry_eeg_completion'].value_counts()[8]
 
-print "ready for S5 call:", s5_sched.loc['tp1_day1_arm_1'][3]
-print ''
+proc = PrettyTable()
 
-# NEED OT FIX:
-print "Completed Tp1- Treatment in progress:", tp1_complete.loc['tp1_day1_arm_1'][1] - \
-    therapy_completed[1] - therapy_completed[2] - tp2_sched[2]
+proc.field_names = ['-', 'Intake', 'Dry EEG',
+                    'Saline EEG', 'VMREACT', 'Webneuro', 'fMRI', 'TMS/EEG']
+proc.add_row(["Psychotherapy Tp1", intake_pt_tp1, dry_pt_tp1, saline_pt_tp1,
+              vm_react_pt_tp1, webneuro_pt_tp1, mri_pt_tp1, tms_pt_tp1])
+proc.add_row(["Psychotherapy Tp2", intake_pt_tp2, dry_pt_tp2, saline_pt_tp2,
+              vm_react_pt_tp2, webneuro_pt_tp2, mri_pt_tp2, tms_pt_tp2])
+proc.add_row(["Healthy Veterans", intake_hv, dry_hv_tp1,
+              saline_hv_tp1, vm_react_hv, webneuro_hv, mri_hv, tms_hv])
 
-print "Completed Treatment- Tp2 scheduling in progress:", tp2_sched.loc[
-    'tp1_day1_arm_1'][2] - tp2_sched.loc['tp1_day1_arm_1'][3]
+table = PrettyTable()
 
-print "Completed Treatment- Tp2 scheduled:", tp2_sched.loc['tp1_day1_arm_1'][1] - \
-    tp2_complete.loc['tp1_day1_arm_1'][1] - tp2_sched.loc['tp1_day1_arm_1'][3]
+table.field_names = ['-', 'Tp1 Completions',
+                     'Tp2 Completions', '6 Month Followup']
+table.add_row(["Psychotherapy", tp1_completed_patients,
+               tp2_completed_patients, six_month_followup])
+table.add_row(["Healthy Veterans", tp1_completed_hv, '-', '-'])
+table.add_row(["EEG/fMRI Healthy Controls",
+               tp1_completed_hc, tp2_completed_hc, '-'])
 
-print ''
+#
+#print 'PT TP1 completed',tp1_completed_patients
+#print 'PT TP2 completed',tp2_completed_patients
+#print '6 month followup', six_month_followup
+#print 'HV TP1 completed', tp1_completed_hv
+#print 'HC TP1 completed', tp1_completed_hc
+#print 'HC TP2 completed',tp2_completed_hc
+#
+#
+#print 'pt tms',tms_pt
+#print 'pt fmri',mri_pt
+#print 'tp1 pt saline',saline_pt_tp1
+#print 'pt dry tp1',dry_pt_tp1
+#print 'hv dry',dry_hv_tp1
+#print 'hv mri',mri_hv
+#print 'hv saline',saline_hv_tp1
+#print 'hc tp1 saline',saline_hc_tp1
+#print 'tp2 pt saline',saline_pt_tp2
 
-print "Total dropped out of treatmnet:", pt_dropped_therapy.loc['tp1_day1_arm_1'][1] + \
-    pt_dropped_unreachable.loc['tp1_day1_arm_1'][1] + \
-    pt_switched_modals.loc['tp1_day1_arm_1'][1]
-
-print "Dropped out of Treatment- Collected Tp2 clinical measures:", tp2_complete[2]
-print "Dropped out of Treatment- Collected Tp2 Data:",
-print "Dropped out of Treatment- No Tp2 Data", tp2_complete[2]
-print "Withdrawn or Withdrew:", withdrawn_withdrew.loc['tp1_day1_arm_1'][1]
+print(table)
+print('')
+print (proc)
